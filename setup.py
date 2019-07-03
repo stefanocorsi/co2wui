415ea98ccc872122601743a7d02457a4b98abdb8
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-03 14:39:31 +0200

    feat(build): +`test` extras in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+
+test_deps = ["pytest", "selenium"]
 setup(
     name="co2wui",
     version="0.0.1.dev0",
@@ -17,13 +19,15 @@ setup(
         "ruamel.yaml",
         "syncing",
     ],
-    extras={
-        "dev": [
+    extras_require={
+        "test": test_deps,
+        "dev": test_deps
+        + [
             "black",  # for code-formatting
             "pip",
             "pre-commit",  # for code-formatting
             "wheel",
-        ]
+        ],
     },
     entry_points={"console_scripts": ["co2wui=co2wui.app:cli"]},
 )
