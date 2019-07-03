b7849861ea3fdfd5d8a3dc837b0ec328fc8969b0
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-03 14:42:08 +0200

    enh(build): typical extravaganca for setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
 
+def read(fpath):
+    with open(fpath) as fp:
+        return fp.read()
+
+
 test_deps = ["pytest", "selenium"]
 setup(
     name="co2wui",
     version="0.0.1.dev0",
     packages=["co2wui"],
-    license="EUPL",
-    long_description=open("README.md").read(),
+    license="European Union Public Licence 1.1 or later (EUPL 1.1+)",
+    description="WebUI for co2mpas",
+    long_description=read("README.md"),
+    keywords=["automotive", "vehicles", "simulator", "WLTP", "web-app"],
+    project_urls={
+        "Documentation": "https://co2mpas.io/",
+        "Sources": "https://github.com/JRCSTU/co2wui",
+    },
     install_requires=[
         "co2mpas",
         "Flask",
@@ -30,4 +41,27 @@ setup(
         ],
     },
     entry_points={"console_scripts": ["co2wui=co2wui.app:cli"]},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Manufacturing",
+        "Environment :: Console",
+        "License :: OSI Approved :: European Union Public Licence 1.1 " "(EUPL 1.1)",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
 )
