10ed2269e383c51ad423ccd461c90263bc9b4cfe
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-03 21:59:14 +0200

    FEAT(conda): +recipe with Win menu-items (UNTESTED)
    + __main__ to facilitate Win manu-items (from conda) & PEP366

---
+++ co2wui/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019-2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+from co2wui import app
+
+if __name__ == "__main__":
+    # ... normally `cli` is a separate module containing `main()` function ...
+    app.cli()
