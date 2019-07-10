f8dca94dcd5994d611be8a08c01ea300b23c173e
Stefano Corsi <stefano.corsi@gmail.com>
2019-07-10 15:41:28 +0200

    feat(test): add ui tests for datasync #55

--- test/ui/test_co2wui.py
+++ test/ui/test_co2wui.py
@@ -14,7 +14,7 @@ from selenium.webdriver.support.ui import Select
 
 class TestCo2mpasWui(unittest.TestCase):
     def setUp(self):
-        print("setUp executed")
+        print("\nsetUp executed")
         opts = Options()
         opts.headless = True
 
@@ -33,7 +33,7 @@ class TestCo2mpasWui(unittest.TestCase):
     def test_100_datasync_form(self):
 
         driver = self.driver
-        print("Starting datasync UI test")
+        print("Starting datasync download template UI test")
         driver.get("http://localhost:5000/sync/template-form")
 
         elem = driver.find_element_by_tag_name("h1")
@@ -54,6 +54,157 @@ class TestCo2mpasWui(unittest.TestCase):
         select_element.select_by_value("nedc")
         elem = driver.find_element_by_id("gearbox")
         self.assertEqual(elem.text, "Automatic\nManual")
+
+    def test_120_datasync(self):
+        driver = self.driver
+        print("Starting datasync UI test")
+        driver.get("http://localhost:5000/sync/synchronisation-form")
+
+        elem = driver.find_element_by_id("synchronise-button")
+        self.assertEqual(elem.text, "Synchronise data")
+
+        elem.click()
+        WebDriverWait(self.driver, 5).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, "sync-feedback"), "1 file has been processed"
+            )
+        )
+
+    def test_130_new_sync(self):
+
+        driver = self.driver
+        print("Starting new synchronisation UI test")
+        driver.get("http://localhost:5000/sync/synchronisation-form")
+
+        elem = driver.find_element_by_id("synchronise-button")
+        self.assertEqual(elem.text, "Synchronise data")
+
+        elem.click()
+        WebDriverWait(self.driver, 5).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, "sync-feedback"), "1 file has been processed"
+            )
+        )
+
+        elem = driver.find_element_by_id("synchronise-button")
+        assert elem.is_displayed() is not True
+
+        elem = driver.find_element_by_link_text("Synchronise another file")
+        elem.click()
+
+        elem = driver.find_element_by_id("synchronise-button")
+        assert elem.is_displayed() is True
+
+    def test_140_delete_file_and_upload(self):
+
+        driver = self.driver
+        print("Starting datasync delete file UI test")
+        driver.get("http://localhost:5000/sync/synchronisation-form")
+
+        src = driver.page_source
+        text_found = re.search(r"datasync.xlsx", src)
+        self.assertNotEqual(text_found, None)
+
+        elem = driver.find_element_by_id("delete-button")
+        elem.click()
+
+        src = driver.page_source
+        text_found = re.search(r"datasync.xlsx", src)
+        self.assertEqual(text_found, None)
+
+        elem = driver.find_element_by_id("file")
+        elem.send_keys(os.path.join(os.getcwd(), "test", "datasync.xlsx"))
+
+        elem = driver.find_element_by_id("add-sync-file-form").submit()
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(EC.visibility_of_element_located((By.ID, "delete-button")))
+
+        src = driver.page_source
+        text_found = re.search(r"datasync.xlsx", src)
+        self.assertNotEqual(text_found, None)
+
+    def test_150_datasync_file_other_name(self):
+
+        driver = self.driver
+        print("Starting datasync other name UI test")
+        driver.get("http://localhost:5000/sync/synchronisation-form")
+
+        elem = driver.find_element_by_id("delete-button")
+        elem.click()
+
+        elem = driver.find_element_by_id("file")
+        elem.send_keys(os.path.join(os.getcwd(), "test", "datasync-other-name.xlsx"))
+
+        elem = driver.find_element_by_id("add-sync-file-form").submit()
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(EC.visibility_of_element_located((By.ID, "delete-button")))
+
+        src = driver.page_source
+        text_found = re.search(r"datasync-other-name.xlsx", src)
+        self.assertNotEqual(text_found, None)
+
+        src = driver.page_source
+        text_found = re.search(r"datasync.xlsx", src)
+        self.assertEqual(text_found, None)
+
+        elem = driver.find_element_by_id("delete-button")
+        elem.click()
+
+        elem = driver.find_element_by_id("file")
+        elem.send_keys(os.path.join(os.getcwd(), "test", "datasync.xlsx"))
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(EC.visibility_of_element_located((By.ID, "delete-button")))
+
+        src = driver.page_source
+        text_found = re.search(r"datasync.xlsx", src)
+        self.assertNotEqual(text_found, None)
+
+    def test_160_datasync_with_empty_file(self):
+
+        driver = self.driver
+        print("Starting datasync with empty file UI test")
+        driver.get("http://localhost:5000/sync/synchronisation-form")
+
+        elem = driver.find_element_by_id("delete-button")
+        elem.click()
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(
+            EC.visibility_of_element_located((By.ID, "add-sync-file-form"))
+        )
+
+        elem = driver.find_element_by_id("file")
+        elem.send_keys(os.path.join(os.getcwd(), "test", "datasync-empty.xlsx"))
+
+        driver.implicitly_wait(2)
+        elem = driver.find_element_by_id("add-sync-file-form").submit()
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(EC.visibility_of_element_located((By.ID, "delete-button")))
+
+        elem = driver.find_element_by_id("synchronise-button")
+        self.assertEqual(elem.text, "Synchronise data")
+
+        elem.click()
+        WebDriverWait(self.driver, 5).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, "logarea"), "Synchronisation failed:"
+            )
+        )
+
+        elem = driver.find_element_by_id("delete-button")
+        elem.click()
+
+        elem = driver.find_element_by_id("file")
+        elem.send_keys(os.path.join(os.getcwd(), "test", "datasync.xlsx"))
+
+        elem = driver.find_element_by_id("add-sync-file-form").submit()
+
+        wait = WebDriverWait(driver, 10)
+        cond = wait.until(EC.visibility_of_element_located((By.ID, "delete-button")))
 
     def test_200_generate_config_file(self):
 
