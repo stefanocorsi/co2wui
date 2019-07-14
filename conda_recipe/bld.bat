cc358dc784a1c43d13fbab6e9023ddc49fbfb5ea
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-14 17:38:50 +0200

    DROP(conda): recipe in jrcstu-forge repo...
    no point in being here, if it is to be submitted in conda-forge.

--- conda_recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\co2wui.json"
-copy "%RECIPE_DIR%\menu.ico" "%PREFIX%\Menu\co2wui.ico"
-
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vv
-if errorlevel 1 exit 1
--- conda_recipe/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p "$PREFIX/Menu"
-cp "$RECIPE_DIR/menu.json" "$PREFIX/Menu/co2wui.json"
-cp "$RECIPE_DIR/menu.ico" "$PREFIX/Menu/co2wui.ico"
-
-"$PYTHON" -m pip install . --no-deps --ignore-installed -vv || exit 1
